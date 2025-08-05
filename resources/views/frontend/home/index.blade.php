0ba534ed3896b1444479d39b340f8b21ac0c28f0
Aabishkar Shrestha <Aabishkar@adsaro.com>
2025-08-05 10:41:30 +0545

    first commit

---
+++ resources/views/frontend/home/index.blade.php
@@ -0,0 +1,12 @@
+@extends('frontend.layout.app')
+@section('body')
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h1>Welcome to the Home Page</h1>
+            <p>This is the content of the home page.</p>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/frontend/layout/app.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="{{ $page->language ?? 'en' }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Accomodation</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
+        integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="icon" href="" type="image/x-icon">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
+    </style>
+    <style>
+        body {
+            font-family: "Montserrat", serif !important;
+            font-weight: 400;
+            font-style: normal;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="">
+        @include('frontend.layout.navbar')
+        <div class="">
+            @yield('body')
+        </div>
+
+        <div class="">
+            @include('frontend.layout.footer')
+        </div>
+
+    </div>
+</body>
+
+</html>
