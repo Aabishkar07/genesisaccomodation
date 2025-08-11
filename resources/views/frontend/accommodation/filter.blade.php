f8a19a026aad41355fb18dc09eb254ff32b882d0
Aabishkar Shrestha <Aabishkar@adsaro.com>
2025-08-11 13:15:08 +0545

    Updated

--- resources/views/frontend/accommodation/filter.blade.php
+++ resources/views/frontend/accommodation/filter.blade.php
@@ -1,9 +1,6 @@
 @extends('frontend.layout.app')
 @section('body')
-
-
-   <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3  my-12 max-w-screen-2xl mx-auto px-4">
-            @include('frontend.component.accomodation')
-        </div>
-
-    @endsection
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3  my-12 max-w-screen-2xl mx-auto px-4">
+        @include('frontend.component.accomodation')
+    </div>
+@endsection
